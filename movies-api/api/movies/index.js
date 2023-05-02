2022538f24a535725a4f3b418ca05491b8c93dd5
unknown <chitasjessica@gmail.com>
2023-05-02 23:40:03 +0100

    Topic 12 - Finished!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
+import { getUpcomingMovies } from '../tmdb-api';
 
 const router = express.Router(); 
 
@@ -37,6 +38,11 @@ router.get('/:id/reviews', (req, res) => {
         });
     }
 });
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
 
 //Post a movie review
 router.post('/:id/reviews', (req, res) => {
