edc2743cc6384ed242ddbfdb649da45e6a11cfb0
unknown <chitasjessica@gmail.com>
2023-05-02 22:47:19 +0100

    Topic 12 - Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
 
 const Schema = mongoose.Schema;
 
@@ -13,4 +14,38 @@ const MovieSchema = new Schema({
     favourites: [MovieSchema]
   });
 
+  UserSchema.methods.comparePassword = function (passw, callback) {
+    bcrypt.compare(passw, this.password, (err, isMatch) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, isMatch);
+    });
+  };
+
+  UserSchema.statics.findByUserName = function (username) {
+    return this.findOne({ username: username });
+  };
+  
+  UserSchema.pre('save', function(next) {
+    const user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, (err, salt)=> {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, null, (err, hash)=> {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+  
 export default mongoose.model('User', UserSchema);
