642dd44baffc4d9740affad1c4aa3307cf173e66
unknown <chitasjessica@gmail.com>
2023-05-02 22:52:15 +0100

    Topic 12 - JWT

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+
 const router = express.Router(); // eslint-disable-line
 
 // Get all users
@@ -18,25 +20,30 @@ router.get('/:id/favourites', async (req, res) => {
     }
 });
 
-
-  // register(Create)/Authenticate User
-  router.post('/', async (req, res) => {
-    if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successful created new user.',
+// Register OR authenticate a user
+router.post('/',asyncHandler( async (req, res, next) => {
+    if (!req.body.username || !req.body.password) {
+      res.status(401).json({success: false, msg: 'Please pass username and password.'});
+      return next();
+    }
+    if (req.query.action === 'register') {
+      await User.create(req.body);
+      res.status(201).json({code: 201, msg: 'Successful created new user.'});
+    } else {
+      const user = await User.findByUserName(req.body.username);
+        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+        user.comparePassword(req.body.password, (err, isMatch) => {
+          if (isMatch && !err) {
+            // if user is found and password matches, create a token
+            const token = jwt.sign(user.username, process.env.SECRET);
+            // return the information including token as JSON
+            res.status(200).json({success: true, token: 'BEARER ' + token});
+          } else {
+            res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
+          }
         });
-    }
-    else {  //Must be an authenticate then!!! Query the DB and check if there's a match
-        const user = await User.findOne(req.body);
-        if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-        }else{
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
-        }
-    }
-});
+      }
+  }));
 
 // favourites
 router.post('/:id/favourites', async (req, res) => {
@@ -52,25 +59,6 @@ router.post('/:id/favourites', async (req, res) => {
         }
     }
 });
-
-  // register
-  router.post('/', asyncHandler(async (req, res) => {
-    if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save()
-        res.status(201).json({
-            code: 201,
-            msg: 'Successful created new user.',
-        });
-    }
-    else {  //Must be authenticating then! Query the DB and check if there's a match
-        const user = await User.findOne(req.body);
-        if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
-        } else {
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
-        }
-    }
-}));
 
  // Update a user
  router.put('/:id', async (req, res) => {
